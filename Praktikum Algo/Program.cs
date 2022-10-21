15d81472ac4ec3333f401e4309927ef8589a72ff
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2022-10-21 16:52:40 +0700

    Add project files.

---
+++ Praktikum Algo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktikum_Algo
+{
+     class Program
+    {
+       // quick sort
+        //Array of of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count - 0; // number of data movements
+
+        // number of element in array
+        private int n;
+    }
+}
