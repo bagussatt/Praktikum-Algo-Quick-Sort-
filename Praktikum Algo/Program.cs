fcac683fd2961e839c8fe9d4e3e66caafe452acc
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2022-10-21 16:54:16 +0700

    //Array of of integers to hold values

--- Praktikum Algo/Program.cs
+++ Praktikum Algo/Program.cs
@@ -12,7 +12,7 @@ namespace Praktikum_Algo
         //Array of of integers to hold values
         private int[] arr = new int[20];
         private int cmp_count = 0; // number of comparasion
-        private int mov_count - 0; // number of data movements
+        private int mov_count = 0; // number of data movements
 
         // number of element in array
         private int n;
